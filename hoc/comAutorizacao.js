264d611184e5f67b2a9ad8f4edfe170c91a97995
Caickolas <88976384+Caickolas@users.noreply.github.com>
2022-03-29 16:19:16 -0300

    alguns ajustes

---
+++ hoc/comAutorizacao.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/display-name */
+import { useRouter } from "next/router";
+
+export default function comAutorizacao(Componente) {
+    const estaLogado = true;
+    
+    return (props) => {
+        if(typeof window === 'undefined') {
+            return null;
+        }
+
+
+        const router = useRouter();
+
+
+        if(estaLogado) {
+            return <Componente {...props} />
+        }
+        router.push('/login')
+    }
+}
